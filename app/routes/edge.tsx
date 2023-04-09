3602cb48a991e9d581d849c53489ab440f50ed8e
John Griffin <john@griffin.io>
2023-04-09 16:35:38 -0400

    Show sample env var on root and edge routes

--- app/routes/edge.tsx
+++ app/routes/edge.tsx
@@ -1,9 +1,18 @@
+import { useLoaderData } from "@remix-run/react";
+import { LoaderArgs, json } from "@vercel/remix";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const sampleEnvVar = process.env.SAMPLE_ENV_VAR;
+  return json({ sampleEnvVar });
+}
+
 export const config = { runtime: "edge" };
 
 export default function Edge() {
+  const { sampleEnvVar } = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix@Edge</h1>
+      Sample Env Var: <code>{sampleEnvVar}</code>
     </div>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,16 @@
+import { useLoaderData } from "@remix-run/react";
+import { LoaderArgs, json } from "@vercel/remix";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const sampleEnvVar = process.env.SAMPLE_ENV_VAR;
+  return json({ sampleEnvVar });
+}
+
 export default function Index() {
+  const { sampleEnvVar } = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      Sample Env Var: <code>{sampleEnvVar}</code>
     </div>
   );
 }
